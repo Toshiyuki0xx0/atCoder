13d862043ad108684f8b7f38fda0930d0fdbfbb8
Toshiyuki0xx0 <t_toshiyuki@td5.so-net.ne.jp>
2020-09-26 19:19:11 +0900

    てすと

--- src/dream/Main.java
+++ src/dream/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		//
 		// TODO 自動生成されたメソッド・スタブ
 		Scanner sc = new Scanner(System.in);
 		String S = sc.next();
